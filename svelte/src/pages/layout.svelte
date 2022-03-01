6d57ead9fda3bda4fa01fbee5514726c75bf0f08
Doug Day <dday@mastercontrol.com>
2022-02-28 20:35:47 -0700

    Cleanup event listeners a bit with bind:this

--- svelte/src/pages/layout.svelte
+++ svelte/src/pages/layout.svelte
@@ -7,44 +7,41 @@
 
     import { onMount, onDestroy } from "svelte";
 
-    let drawer: Drawer = null;
+    let drawer: Drawer;
     const handleNavigationClick = () => {
         drawer.open = !drawer.open;
     };
 
     onMount(async () => {
-        drawer = document.getElementsByTagName("mwc-drawer")[0];
-        drawer.parentNode.addEventListener("MDCTopAppBar:nav", handleNavigationClick);
+        addEventListener("MDCTopAppBar:nav", handleNavigationClick);
     });
 
     onDestroy(() => {
-        drawer.parentNode.removeEventListener("MDCTopAppBar:nav", handleNavigationClick);
+        removeEventListener("MDCTopAppBar:nav", handleNavigationClick);
     });
 </script>
 
-<main>
-    <mwc-drawer hasHeader type="modal">
-        <span slot="title">Hello World!</span>
-        <div>
-            <ul>
-                <li>
-                    <a href="#/"><mwc-icon>home</mwc-icon> Home</a>
-                </li>
-                <li>
-                    <a href="#/hello-world"><mwc-icon>language</mwc-icon> Hello World!</a>
-                </li>
-            </ul>
-        </div>
-        <div slot="appContent">
-            <mwc-top-app-bar-fixed dense>
-                <slot name="title" slot="title" />
-                <slot name="actionItems" slot="actionItems" />
-                <mwc-icon-button icon="menu" slot="navigationIcon" />
-                <slot />
-            </mwc-top-app-bar-fixed>
-        </div>
-    </mwc-drawer>
-</main>
+<mwc-drawer bind:this={drawer} hasHeader type="modal">
+    <span slot="title">Hello World!</span>
+    <div>
+        <ul>
+            <li>
+                <a href="#/"><mwc-icon>home</mwc-icon> Home</a>
+            </li>
+            <li>
+                <a href="#/hello-world"><mwc-icon>language</mwc-icon> Hello World!</a>
+            </li>
+        </ul>
+    </div>
+    <div slot="appContent">
+        <mwc-top-app-bar-fixed dense>
+            <slot name="title" slot="title" />
+            <slot name="actionItems" slot="actionItems" />
+            <mwc-icon-button icon="menu" slot="navigationIcon" />
+            <slot />
+        </mwc-top-app-bar-fixed>
+    </div>
+</mwc-drawer>
 
 <style>
 </style>
