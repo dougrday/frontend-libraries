1b911bac717fa0172e343ab608bc25ae112e76dd
Doug Day <dday@mastercontrol.com>
2022-03-04 17:09:20 -0700

    Added a simple, basic home page

--- svelte/src/pages/Home.svelte
+++ svelte/src/pages/Home.svelte
@@ -3,14 +3,27 @@
 </script>
 
 <Layout>
-    <div slot="title">Home</div>
+    <div slot="title">Home(r)</div>
     <div slot="actionItems">
         <mwc-icon-button icon="file_download" />
         <mwc-icon-button icon="print" />
         <mwc-icon-button icon="favorite" />
     </div>
-    Home
+
+    <div class="centered">
+        <img alt="Homer" src="/images/homer.jpg" />
+        <a href="#/hello-world">Hello, World!</a>
+    </div>
 </Layout>
 
 <style>
+    .centered {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+    }
+    img {
+        width: 400px;
+    }
 </style>
